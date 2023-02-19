0cb2d5e8dd2c6780ecaec61ef3a86f685d4697e7
Denis Zuev <denis.v.zuev@yandex.ru>
2023-02-19 20:54:58 +0300

    Task 3-3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,17 @@
 function camelize(str) {
-  // ваш код...
+  return str.split('-').map((item, index) => index > 0 ? ucFirst(item) : item).join('');
+}
+
+
+function ucFirst(str) {
+
+  if (str === null || str === undefined){
+      return;
+    } else if (str.length <= 1){
+      return str.toUpperCase();
+    }
+    else {
+     return str[0].toUpperCase() + str.slice(1);
+    }
+
 }
