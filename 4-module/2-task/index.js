a23acc7f45005b7eabfd415a0930b830d9b099d8
Denis Zuev <denis.v.zuev@yandex.ru>
2023-02-21 00:47:21 +0300

    Task 4-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for(let i = 0; i < table.rows.length; i++){
+    if (i < table.rows[i].children.length) {
+      table.rows[i].children[i].style = 'background: red';
+    }
+  }
+
 }
